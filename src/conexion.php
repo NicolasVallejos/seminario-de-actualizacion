cfefc16c1a7764020cb9380acb82e0c3f8fe60f7
NicolasVallejos <134241363+NicolasVallejos@users.noreply.github.com>
2024-05-15 19:11:07 -0300

    Delete directory

--- src/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("127.0.0.1","root","","contacto");
-	
-	if($conn->connect_errno)
-	{
-		echo "No hay conexión: (" . $conn->connect_errno . ") " . $conn->connect_error;
-	}
-?>
